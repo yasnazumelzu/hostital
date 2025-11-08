8d7bd07b676556fb27100fafecc78aa42ae35d84
yasnazumelzu <yasna.zumelzu@inacapmail.cl>
2025-11-08 17:44:31 -0300

    istalacion de requeriments

--- hospital/settings.py
+++ hospital/settings.py
@@ -105,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-es'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'santiago/Chile'
 
 USE_I18N = True
 
